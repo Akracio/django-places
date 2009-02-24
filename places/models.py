3e56f9cdffaeff781d1dcc33f169f7b35f3b05c7
palewire <ben.welsh@latimes.com>
2009-02-23 22:57:56 -0800

    Added Point model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Place(models.Model):
+	"""
+	A point. Maybe a city. Maybe a pothole.
+	"""
+	name = models.CharField(max_length=500)
+	slug = models.SlugField(unique=True)
+	description = models.TextField()
+	point = models.PointField(null=True, blank=True)
+	objects = models.GeoManager()
+	
+	class Meta:
+		ordering = ('name',)
+		
+	def __unicode__(self):
+		u'%s (%4f, %4f)' % (self.name, self.point.x, self.point.y)
